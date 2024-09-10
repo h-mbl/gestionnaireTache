58d5873577c90bdee9309a46467c5727956c9ea9
herve.ngisse <125308992+h-mbl@users.noreply.github.com>
2024-09-10 19:28:36 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path'); // Ajoutez cette ligne
 const server = express();
 require('dotenv').config();
 require('./connexion/connect');
@@ -12,11 +13,20 @@ server.use(cors());
 server.use("/api/user", UserAPI);
 server.use("/api/tache", TaskAPI);
 
-server.use("/", (req, res) => {
-    res.send("Backend is running!");
+// Servir les fichiers statiques du frontend
+server.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Toutes les routes non-API seront redirigées vers l'index.html du frontend
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-const PORT = 1000;
+// Supprimez ou commentez cette route car elle interfère avec le frontend
+// server.use("/", (req, res) => {
+//     res.send("Backend is running!");
+// });
+
+const PORT = process.env.PORT || 1000;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
