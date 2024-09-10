db5c893bee6b81fe0b48920bbb8ad0f09766ac8a
herve.ngisse <125308992+h-mbl@users.noreply.github.com>
2024-09-10 19:53:32 -0400

    Revert "Update server.js"
    This reverts commit 58d5873577c90bdee9309a46467c5727956c9ea9.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path'); // Ajoutez cette ligne
 const server = express();
 require('dotenv').config();
 require('./connexion/connect');
@@ -13,20 +12,11 @@ server.use(cors());
 server.use("/api/user", UserAPI);
 server.use("/api/tache", TaskAPI);
 
-// Servir les fichiers statiques du frontend
-server.use(express.static(path.join(__dirname, '../frontend/build')));
-
-// Toutes les routes non-API seront redirigées vers l'index.html du frontend
-server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+server.use("/", (req, res) => {
+    res.send("Backend is running!");
 });
 
-// Supprimez ou commentez cette route car elle interfère avec le frontend
-// server.use("/", (req, res) => {
-//     res.send("Backend is running!");
-// });
-
-const PORT = process.env.PORT || 1000;
+const PORT = 1000;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
